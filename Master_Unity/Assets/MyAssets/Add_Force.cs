9beb6c60970762659c3aad2036313b6e68ff576d
Justor <justyna.mackos@gmail.com>
2020-05-23 19:16:44 +0200

    restitution and friction coefficients added to phys. materials

---
+++ Master_Unity/Assets/MyAssets/Add_Force.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Add_Force : MonoBehaviour
+{
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(thrust, 0, 0, ForceMode.Impulse);
+    }
+}
